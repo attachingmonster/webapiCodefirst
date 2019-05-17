28f2d88e04b75033dba2b572e7d239d1e479feb4
attachingmonster <3550462318@qq.com>
2019-05-17 12:03:40 +0800

    控制器得到SysRole表的内容

---
+++ webapiCodefirst/webapiCodefirst/Controllers/FirstController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using webapiCodefirst.DAL;
+using webapiCodefirst.Models;
+
+namespace webapiCodefirst.Controllers
+{
+    public class FirstController : ApiController
+    {
+        
+        private AccountContext db = new AccountContext();
+        private UnitOfWork unitOfWork = new UnitOfWork();
+        [HttpGet]
+        [ActionName("GetSysRoles")]
+        public List<SysRole> Get()
+        {
+            var sysRoles = (unitOfWork.SysRoleRepository.Get()).ToList();
+            return sysRoles;
+        }
+
+
+    }
+}
